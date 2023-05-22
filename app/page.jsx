e65531c65734a6259210692df721c8ea3b4bd441
Aissa <semaoui0347@gmail.com>
2023-05-22 16:44:40 +0100

    change logo pos, remove h2, changes on the layout

--- app/page.jsx
+++ app/page.jsx
@@ -5,22 +5,23 @@ import MainSearchBox from "./components/MainSearchBox";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen flex-col bg-green- justify-center items-center gap-2 p-4 md:p-12 lg:p-24 bg-gradient">
-      <Image
-        src="./next.svg"
-        width={150}
-        height={150}
-        className="mb-10"
-      ></Image>
-      <div className="max-w-2xl w-full">
-        <h1 className="text-2xl text-center sm:text-3xl md:text-4xl text-white font-[900] mb-2">
-          What are you looking for!
-        </h1>
-        <h2 className="text-lg md:text-xl text-white font">
-          Better for you, Good for us, Better for accommodations...
-        </h2>
+    <main>
+      <div className="p-8">
+        <Image
+          src="./next.svg"
+          height={0}
+          width={0}
+          className="object-contain w-16 md:w-24"
+        ></Image>
       </div>
-      <MainSearchBox className=" w-full max-w-2xl  mb-72" />
+      <div className="flex w-full h-full flex-col justify-center items-center gap-2 mx-auto p-4 md:py-12 lg:py-24 bg-gradient mt-[10vh]">
+        <div className="max-w-2xl w-full">
+          <h1 className="text-2xl text-center sm:text-3xl md:text-4xl text-white font-[900] mb-6">
+            What are you looking for!
+          </h1>
+        </div>
+        <MainSearchBox className=" w-full max-w-2xl" />
+      </div>
     </main>
   );
 }
