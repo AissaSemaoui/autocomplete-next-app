556b91b4519225134210fb23b0b48517feefd4bd
Aissa <semaoui0347@gmail.com>
2023-05-24 00:04:30 +0100

    hover effect and headers space

--- app/components/Autocomplete/Autocomplete.jsx
+++ app/components/Autocomplete/Autocomplete.jsx
@@ -19,9 +19,9 @@ export function Autocomplete(props) {
     const search = autocomplete({
       container: containerRef.current,
       classNames: {
-        form: "!rounded-full shadow-md !w-full !px-2 !py-2 !border-none !outline-none focus-within:!shadow-none",
+        form: "!rounded-full hover:shadow-md !w-full !px-2 !py-2 !border-none !outline-none focus-within:!shadow-none",
         detachedSearchButton:
-          "!rounded-full shadow-md !w-full !px-2 !py-2 !border-none !outline-none focus-within:!shadow-none",
+          "!rounded-full hover:shadow-md !w-full !px-2 !py-2 !border-none !outline-none focus-within:!shadow-none",
         panel: "!rounded-xl",
         item: "!rounded-md !p-0 mb-2",
       },
--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       </div>
       <div className="flex w-full h-full flex-col justify-center items-center gap-2 mx-auto p-4 md:py-12 lg:py-24 bg-gradient mt-[10vh]">
         <div className="max-w-3xl w-full">
-          <h1 className="text-2xl text-center sm:text-3xl md:text-4xl text-white font-[900] font-poppins">
+          <h1 className="text-2xl text-center sm:text-3xl md:text-4xl text-white font-[900] font-poppins mb-2">
             Good for us. Better for accommodations.
           </h1>
           <h1 className="text-2xl text-center sm:text-3xl md:text-4xl text-white font-[900] font-poppins mb-6">
