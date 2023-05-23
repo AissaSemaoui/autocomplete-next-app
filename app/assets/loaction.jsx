409c231ab66336b31f069b4796294d531712cfff
Aissa <semaoui0347@gmail.com>
2023-05-23 10:25:59 +0100

    change the icon

--- app/assets/loaction.jsx
+++ app/assets/loaction.jsx
@@ -1,7 +1,7 @@
 export const Location = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-location"
+    class="icon icon-tabler icon-tabler-zzz"
     width="24"
     height="24"
     viewBox="0 0 24 24"
@@ -13,6 +13,7 @@ export const Location = (props) => (
     {...props}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path>
+    <path d="M4 12h6l-6 8h6"></path>
+    <path d="M14 4h6l-6 8h6"></path>
   </svg>
 );
