fa373e160048773826bd43f629cd5fda3ae0ffb8
AissaSemaoui <semaoui0347@gmail.com>
2023-07-07 00:20:25 +0100

    ui improvements

--- app/components/ui/Anchor.jsx
+++ app/components/ui/Anchor.jsx
@@ -5,7 +5,7 @@ function Anchor({ label, link, rightIcon }) {
   return (
     <Link
       href={link}
-      className="group relative block mb-3 text-white text-lg md:text-xl bg-green-500 hover:bg-green-600 active:bg-green-700 w-full py-3 rounded-full duration-75 shadow-sm"
+      className="group relative block mb-3 text-white text-lg md:text-xl bg-green-500 hover:bg-green-600 active:bg-green-700 w-full py-2 rounded-full duration-75 shadow-sm"
     >
       {label} {rightIcon}
     </Link>
